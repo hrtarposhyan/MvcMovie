bbdc0ab935b25e08c0a8bd68d42da73acbb96e91
hrtarposhyan <hrtarposhyan@gmail.com>
2020-03-23 11:40:53 -0700

    Add Controller

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit htaps://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my defaul action...";
+
+        }
+        // 
+        // GET: /HelloWorld/Welcome/
+
+        //public string Welcome(string name, int numTimes=1)
+
+        //{
+        //    return "This is the Welcome action method...";
+
+        //}
+        //public string Welcome(string name, int numTimes = 1)
+
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public string Welcome(string name, int ID = 1)
+
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
